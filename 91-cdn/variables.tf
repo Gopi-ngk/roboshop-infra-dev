446ef3ae8f44be1d025b1896fb3bcfe3d55425ac
Gopi <gopikuchi1827@gmail.com>
2025-11-21 18:13:10 +0530

    terraform

---
+++ 91-cdn/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00345272KTJ7HVI6EF6V"
+}
+
+variable "zone_name" {
+    default = "gopinath.store"
+}
